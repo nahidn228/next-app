1f24c9cd689d1da6d95160ca6d0d7c1d7efcf5bd
nahidn228 <nahidn228@gmail.com>
2025-09-20 11:47:49 +0600

    Handle global errors with built-in error page

---
+++ src/app/product/loading.tsx
@@ -0,0 +1,12 @@
+
+
+//this is loading state for this page
+const LoadingPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-5xl"> loading...</h1>
+    </div>
+  );
+};
+
+export default LoadingPage;
