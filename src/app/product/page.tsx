2ecc6a1dd2e8f85088a9f68c1034caf28da00f58
nahidn228 <nahidn228@gmail.com>
2025-09-20 11:10:28 +0600

    implement data fatching using force-cache

---
+++ src/app/product/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "All products",
+};
+
+const ProductPage = async () => {
+  const res = await fetch("http://localhost:5000/products", {
+    cache: "force-cache",
+  });
+  const products = await res.json();
+  console.log(products);
+  return (
+    <div>
+      <h1>This is Product Page component {products?.length} </h1>
+    </div>
+  );
+};
+
+export default ProductPage;
